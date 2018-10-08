ddde382887bd4459cd51f1561155fe123fc14cbe
Blokhin Andrew <intestinesonastick@gmail.com>
2018-10-08 22:46:59 +0300

    Not red msg highlighting fixed. + Bot-welcome fix

--- client/app/directives/check_msg_was_seen.directive.js
+++ client/app/directives/check_msg_was_seen.directive.js
@@ -9,14 +9,16 @@ angular
           ctrl: "=ctrl"
         },
         link: function(scope, elem , attrs){
-          if(!scope.ctrl.atBottom){
+          const UL = Array.from(document.querySelectorAll("#latest-messages > li"));
+          const isNewMsg = UL.indexOf(elem[0]) > scope.ctrl.showOnLoadLimit ;
+          if(!scope.ctrl.atBottom && isNewMsg  ){
             elem[0].style.background = "linear-gradient(to right, #FFF 15%, #71bcf142 100%)"
-          }
+          };
           angular.element(document.getElementById("messages-window")).bind("scroll" , function(e){
             if(scope.ctrl.atBottom){
               $timeout(()=>{
                 elem[0].style.background = "";
-              }, 1000)
+              }, 500)
             }
           })
         }
