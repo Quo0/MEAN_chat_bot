286a37fbf32cd5b9efce10c448b0ad89b3bf2486
Blokhin Andrew <ablokh@mera.ru>
2018-10-17 12:14:13 +0300

    hot fix check_msg_was_seen

--- client/app/directives/check_msg_was_seen.directive.js
+++ client/app/directives/check_msg_was_seen.directive.js
@@ -16,8 +16,9 @@ angular
           if(!atBottom && isNewMsg  ){
             elem[0].style.background = "linear-gradient(to right, #FFF 15%, #71bcf142 100%)"
           };
-          angular.element(document.getElementById("messages-window")).bind("scroll" , function(e){
-            if(atBottom){
+          const messagesWindow = angular.element(document.getElementById("messages-window"));
+          messagesWindow.bind("scroll" , function(e){
+            if( messagesWindow[0].scrollHeight - messagesWindow[0].clientHeight - messagesWindow[0].scrollTop <= 2){
               $timeout(()=>{
                 elem[0].style.background = "";
               }, 500)
@@ -27,43 +28,3 @@ angular
       }
     }
   ]);
-
-
-  // angular
-  //   .module("chatApp")
-  //   .directive("beenSeenStatus", [
-  //     "$timeout",
-  //     function($timeout){
-  //       return {
-  //         restrict: "A",
-  //         scope: {
-  //           // ctrl: "=ctrl",
-  //           // showonloadlimit: "@"
-  //         },
-  //         link: function(scope, elem , attrs){
-  //           const messagesWindow = angular.element(document.getElementById("messages-window"));
-  //           const UL = Array.from(document.querySelectorAll("#latest-messages > li"));
-  //           const isNewMsg = UL.indexOf(elem[0]) > 50 ;
-  //           const bottomPosition = messagesWindow[0].scrollHeight - messagesWindow[0].clientHeight - messagesWindow[0].scrollTop;
-  //           // console.log(bottomPosition - 67);
-  //           // 67 is the "bot typing" msg height
-  //           if(!bottomPosition - 67 <= 2 && isNewMsg  ){
-  //             elem[0].style.background = "linear-gradient(to right, #FFF 15%, #71bcf142 100%)"
-  //           };
-  //           messagesWindow.bind("scroll" , function(e){
-  //             // console.log(bottomPosition);
-  //             if(bottomPosition <= 1){
-  //               $timeout(()=>{
-  //                 elem[0].style.background = "";
-  //               }, 500)
-  //             }
-  //           })
-  //
-  //           scope.$on("$destroy", function(){
-  //             console.log("unbind");
-  //             messagesWindow.unbind("scroll");
-  //           });
-  //         }
-  //       }
-  //     }
-  //   ]);
